5c38e7f8a41ef6ada106b7659149dc8cd8573cb6
Varun <varun@tendercuts.in>
2017-03-28 19:31:34 +0530

    Added logs

--- tendercuts/app/payment_recon/tasks.py
+++ tendercuts/app/payment_recon/tasks.py
@@ -1,9 +1,13 @@
 from .lib.controller import PaymentAutomationController
 from .lib.gateway import Payu
 from config.celery import app
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
 
 @app.task
 def cancel_payu_orders():
-    gw = Payu()
-    controller = PaymentAutomationController(gw)
+
+    gw = Payu(log=logger)
+    controller = PaymentAutomationController(gw, log=logger)
     controller.cancel_pending_orders(dry_run=False)
