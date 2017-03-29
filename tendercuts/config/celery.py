0cab7078ba04ee16af50dfa20234aa6fc0831259
Django <varun@tendercuts.in>
2017-03-29 10:25:20 +0530

    Celery fix: moved it to redis server

--- tendercuts/config/celery.py
+++ tendercuts/config/celery.py
@@ -6,7 +6,7 @@
 from __future__ import absolute_import
 
 from celery import Celery
-from . import celeryconfig
+#from . import celeryconfig
 
 import os, django
 
@@ -14,15 +14,29 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 # django.setup()
 
 app = Celery('tendercuts')
+#app = Celery('tendercuts',
+#             broker='redis://localhost',
+#             backend='redis://localhost')
+
+
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-# app.config_from_object('django.conf:settings', namespace='CELERY')
-app.config_from_object(celeryconfig)
+# app.config_from_object(celeryconfig)
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+from celery.schedules import crontab
+
+app.conf.beat_schedule = {
+    'every-minute': {
+        'task': 'app.payment_recon.tasks.cancel_payu_orders',
+        'schedule': crontab(minute='*/5')
+    },
+}
 
 if __name__ == '__main__':
     app.start()
--- tendercuts/config/settings/celeryconfig.py
+++ tendercuts/config/settings/celeryconfig.py
@@ -4,18 +4,14 @@ from datetime import timedelta
 from celery.schedules import crontab
 
 # moving it here as the root is of no use.
-CELERY_BROKER_URL = 'redis://localhost'
-CELERY_RESULT_BACKEND = 'redis://localhost'
+#CELERY_BROKER_URL = 'redis://localhost:6379/0'
+#CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis+socket:///var/run/redis/redis.sock'
+CELERY_RESULT_BACKEND = 'redis+socket:///var/run/redis/redis.sock'
+
 
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT=['json']
 CELERY_TIMEZONE = 'Asia/Kolkata'
 
-
-CELERYBEAT_SCHEDULE = {
-    'every-minute': {
-        'task': 'app.payment_recon.tasks.cancel_payu_orders',
-        'schedule': crontab(minute='*/5')
-    },
-}
