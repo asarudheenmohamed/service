66d83a9c6c0c1d2439195f4b0a1d69474a408cf6
Varun <varun@tendercuts.in>
2017-04-21 13:20:52 +0530

    Logging enhancements

--- tendercuts/config/settings/base.py
+++ tendercuts/config/settings/base.py
@@ -108,7 +108,7 @@ LOGGING = {
             'level': 'ERROR',
             'propagate': True,
         },
-        'config': {
+        'app': {
             'handlers': ['applogfile',],
             'level': 'DEBUG',
         },
