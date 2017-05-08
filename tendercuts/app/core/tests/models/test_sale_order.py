59b1c79d20042db506fc1ee09caa70fb4b2b1afc
Varun <varun@tendercuts.in>
2017-05-08 15:56:07 +0530

    added promised delivery time

--- tendercuts/app/core/tests/models/test_sale_order.py
+++ tendercuts/app/core/tests/models/test_sale_order.py
@@ -5,4 +5,4 @@ def test_orders(auth_rest):
     order_id = 76106
     orders = SalesFlatOrder.objects.filter(entity_id=order_id)
     assert "9:00-11:00" == orders[0].schedule.ddate.dtimetext
-    assert orders[0].promised_delivery_time() == "Feb 02, Thu 11:00 AM"
+    assert orders[0].promised_delivery_time == "Feb 02, Thu 11:00 AM"
