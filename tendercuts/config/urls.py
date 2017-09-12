83caf373993f8fe719d289bed6cf9d55f9ccd504
Varun <varun@tendercuts.in>
2017-09-12 17:00:35 +0530

    Enable admin panel

--- tendercuts/config/urls.py
+++ tendercuts/config/urls.py
@@ -14,9 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
+# Uncomment the next two lines to enable the admin:
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+
     url(r'core/', include('app.core.urls')),
     url(r'driver/', include('app.driver.urls')),
     url(r'user/', include('app.login.urls')),
