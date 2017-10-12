f460acf7d20095f2d9ee341814f234d143e57a4d
asarudheen <asarudheen@tendercuts.in>
2017-10-12 17:50:43 +0530

    Driver order change view list and admin html creation

---
+++ tendercuts/app/driver/admin/admin.py
@@ -0,0 +1,35 @@
+"""Returns custom objects to the Driver order admin."""
+from django.contrib import admin
+
+from app.core.models.store import CoreStore
+from app.driver.lib.driver_order_controller import StoreOrderController
+from app.driver.models import DriverOrder
+
+
+class DriverOrderAdmin(admin.ModelAdmin):
+    """Driver order change view list."""
+
+    def changelist_view(self, request):
+        """Fetch driver object associated with the corresponding store.
+
+        Args:
+          store_id: store id
+
+        Returns:
+         driver obj
+
+        """
+        data = {}
+        if request.method == 'POST':
+            controller = StoreOrderController()
+            data = controller.get_store_driver_order(request.POST['store_id'])
+
+        data['store_obj'] = CoreStore.objects.all()
+        response = super(DriverOrderAdmin, self).changelist_view(
+            request,
+            extra_context=data
+        )
+
+        return response
+
+admin.site.register(DriverOrder, DriverOrderAdmin)
