1118a188f35c2480a2010c7f175e749c7d6ea875
asarudheen <asarudheen@tendercuts.in>
2017-12-20 19:21:35 +0530

    add logs

--- tendercuts/app/driver/lib/end_of_day_driver_status.py
+++ tendercuts/app/driver/lib/end_of_day_driver_status.py
@@ -21,8 +21,10 @@ class DriverStatusController(object):
 
     def generate_csv(self):
         """Generate a csv file."""
+
         open_file = open('end_of_day_driver_details.csv', 'wb')
         csv_writer = csv.writer(open_file)
+
         logger.debug('csv file generated')
 
         return csv_writer, open_file
@@ -51,6 +53,7 @@ class DriverStatusController(object):
 
     def generate_driver_completed_order_status(self):
         """Generates a csv file that includes driver details worked on current date and send the email for Accounts team."""
+
         # curent date
         start_date = date.today()
 
@@ -58,29 +61,39 @@ class DriverStatusController(object):
         # fetch current date driver order Events who completed order's
         logger.info(
             "Fetch the current date:{} driver order events,who completed order's" .format(start_date))
+
         driver_objects = OrderEvents.objects.filter(
             status='completed', updated_time__range=(start_date, end_date)).prefetch_related('driver')
         # list the order-id's who completed orders
         increment_id = [
             driver_obj.driver.increment_id for driver_obj in driver_objects]
+
         # fetch sale order objects
         logger.info(
             "To fetch the store names based on orders completed by driver")
+
         sale_objects = self.fetch_driver_completed_order_store_names(
             increment_id)
+
         # generate a csv file
         logger.info(
             "To generate a end_of_day_driver_details.csv file")
+
         csv_writer, open_csv_file = self.generate_csv()
+
         # add header row for csv file
         csv_writer.writerow(['Driver name', 'Phone', 'Order id', 'Store Name'])
+
         for driver_obj in driver_objects:
+
             # fetch driver basic info
             driver_basic_info = CustomerSearchController.load_cache_basic_info(
                 driver_obj.driver.driver_id)
+
             # include a row for driver details in csv file
             logger.info(
                 'pushing the driver:{} details along with order id:{} and store name to the csv file.'.format(driver_basic_info['entity_id'], driver_obj.driver.increment_id))
+
             csv_writer.writerow([
                 driver_basic_info['name'],
                 driver_basic_info['phone'],
@@ -89,6 +102,7 @@ class DriverStatusController(object):
                     driver_obj.driver.increment_id)])
 
         open_csv_file.close()
+
         # send email the driver end of day csv file
         Mail().send(
             "reports@tendercuts.in",
@@ -96,3 +110,6 @@ class DriverStatusController(object):
             'Driver details',
             "Here the attachement includes the driver details at end of day",
             ["end_of_day_driver_details.csv"])
+
+        logger.info(
+            "The driver details csv file has been sent to the following mail id's:varun@tendercuts.in,asarudheen@tendercuts.in")
