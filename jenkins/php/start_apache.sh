513d4f61da31d83111baaa9d872a5ef9e597ba9b
Varun <varun@tendercuts.in>
2018-02-12 19:12:55 +0530

    Fix perms for var dir

--- jenkins/php/start_apache.sh
+++ jenkins/php/start_apache.sh
@@ -3,5 +3,6 @@
 echo "Setting env for magento";
 cp /root/local.xml /var/www/html/app/etc/;
 cp /root/constants.php /var/www/html/lib/TenderCutsMessaging/;
+mkdir /var/www/html/var/ && chmod 777 /var/www/html/var/;
 
 service apache2 start;
