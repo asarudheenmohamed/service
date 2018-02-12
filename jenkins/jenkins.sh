46eb69db3587d2440d545a95d473218e53a0c148
Varun <varun@tendercuts.in>
2018-02-12 15:47:22 +0530

    Fix port mapping

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -8,15 +8,15 @@ echo "Installing packages";
 /usr/bin/docker exec -t -u postgres $(/usr/bin/docker ps -aqf "name=django") \
     sh -c "sh /root/entrypoint.sh"
 
-echo "Waiting for env to come up";
-for i in $(seq 1 $END); do
-    result=$(docker exec -t -u root django bash -c "[[ -f '/var/log/django/all_done' ]] || echo 0")
-    if [[ $result -ne 0 ]]; then
-        # all done break out of loop
-        break;
-    fi
-    echo $i;
-done
+#echo "Waiting for env to come up";
+#for i in $(seq 1 $END); do
+#    result=$(docker exec -t -u root django bash -c "[[ -f '/var/log/django/all_done' ]] || echo 0")
+#    if [[ $result -ne 0 ]]; then
+#        # all done break out of loop
+#        break;
+#    fi
+#    echo $i;
+#done
 
 # DB NEEDS TO BE SET UP run replace commands.
 # CLEAN UP CACHE in php.
