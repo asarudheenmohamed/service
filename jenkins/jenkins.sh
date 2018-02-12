cc20e335cc8e78aa6e1691018d4804643d558036
Varun <varun@tendercuts.in>
2018-02-12 15:48:18 +0530

    Fix port mappingl

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -5,7 +5,7 @@ cd $WORKSPACE/jenkins && /usr/local/bin/docker-compose down;
 cd $WORKSPACE/jenkins && /usr/local/bin/docker-compose up -d;
 
 echo "Installing packages";
-/usr/bin/docker exec -t -u postgres $(/usr/bin/docker ps -aqf "name=django") \
+/usr/bin/docker exec -t -u root $(/usr/bin/docker ps -aqf "name=django") \
     sh -c "sh /root/entrypoint.sh"
 
 #echo "Waiting for env to come up";
