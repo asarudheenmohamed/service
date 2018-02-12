1c2c1c062e8c1e5071aea8108c3d325a9d018284
Varun <varun@tendercuts.in>
2018-02-12 18:27:41 +0530

    Start celery in teh same session

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -12,14 +12,6 @@ echo "Installing php package";
 /usr/bin/docker exec -t -u root $(/usr/bin/docker ps -aqf "name=php") \
     sh -c "cd /var/www/html && php composer.phar install"
 
-
-# DB NEEDS TO BE SET UP run replace commands.
-# CLEAN UP CACHE in php.
-
-echo "Starting celery in django container";
-/usr/bin/docker exec -t -u root $(/usr/bin/docker ps -aqf "name=django") \
-    sh -c "cd /services/tendercuts/ && celery -A config worker --loglevel=info --beat &"
-
 echo "Env is up and running, doing all dj migrations";
 /usr/bin/docker exec -t -u root $(/usr/bin/docker ps -aqf "name=django") \
     sh -c "python services/tendercuts/manage.py migrate"
@@ -27,4 +19,4 @@ echo "Env is up and running, doing all dj migrations";
 echo "Starting tests";
 # Log into odoo container and start the tests
 /usr/bin/docker exec -t -u root $(/usr/bin/docker ps -aqf "name=django") \
-    sh -c "cd /services/tendercuts && py.test -vvv . --cache-clear --junitxml=/jenkins/jenkins.xml"
+    sh -c "cd /services/tendercuts && celery -A config worker --loglevel=info --beat & && py.test -vvv . --cache-clear --junitxml=/jenkins/jenkins.xml"
