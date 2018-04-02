d724d897bdbe4368f3af43284f471004fb0d4dc7
Varun <varun@tendercuts.in>
2018-04-02 14:48:44 +0530

    fix docker

--- jenkins/django/entrypoint.sh
+++ jenkins/django/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-pip install -r /services/requirements.txt;
-
 rm -f /var/log/django/all_done;
 # create log dirs
 mkdir -p /var/log/django/ && touch /var/log/django/tendercuts.log;
