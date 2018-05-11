8a57871ae335015925d00cb2cb4cb9709507f282
Varun <varun@tendercuts.in>
2018-05-11 09:04:13 +0530

    Bug fix

--- tendercuts/app/sale_order/lib/order_stat_controller.py
+++ tendercuts/app/sale_order/lib/order_stat_controller.py
@@ -54,7 +54,7 @@ class OrderDataController(object):
                 "discount_description": order.discount_description or '',
                 "payment_method": order.payment.all()[0].method,
                 "medium": order.medium,
-                "spend_amount": reward_amount,
+                "spend_amount": rewards,
 
                 "customer": {
                     "partner_name": order.customer_firstname,
