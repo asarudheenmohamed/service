3cc35caa0444168c1c8c50d4936871aef36fb36b
asarudheen <asarudheen@tendercuts.in>
2018-05-21 14:07:40 +0530

    remove order webhook history update

--- tendercuts/app/core/lib/order_controller.py
+++ tendercuts/app/core/lib/order_controller.py
@@ -5,7 +5,7 @@ magento and python layer
 
 import logging
 
-from app.core.models.sales_order import SalesFlatOrderGrid, SalesFlatOrderStatusHistory
+from app.core.models.sales_order import SalesFlatOrderGrid
 
 from .magento import Connector
 from django.utils import timezone
@@ -87,15 +87,6 @@ class OrderController(object):
 
         self.order.status = status
         self.order.save()
-
-        # update order status in OrderStatusHistory
-        SalesFlatOrderStatusHistory.objects.create(
-            parent=self.order,
-            status=status,
-            created_at=timezone.now(),
-            is_customer_notified=1,
-            is_visible_on_front=1,
-            entity_name='order')
 
         if getattr(self.order, "grid", None):
             self.order.grid.status = "pending"
--- tendercuts/app/core/models/sales_order.py
+++ tendercuts/app/core/models/sales_order.py
@@ -726,22 +726,6 @@ class SalesFlatOrderItem(models.Model):
         app_label = "magento"
 
 
-class SalesFlatOrderStatusHistory(models.Model):
-    entity_id = models.AutoField(primary_key=True)
-    parent = models.ForeignKey('SalesFlatOrder', models.DO_NOTHING)
-    is_customer_notified = models.IntegerField(blank=True, null=True)
-    is_visible_on_front = models.SmallIntegerField()
-    comment = models.TextField(blank=True, null=True)
-    status = models.CharField(max_length=32, blank=True, null=True)
-    created_at = models.DateTimeField(blank=True, null=True)
-    entity_name = models.CharField(max_length=32, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'sales_flat_order_status_history'
-        app_label = "magento"
-
-
 class SalesFlatOrderAddress(models.Model):
     entity_id = models.AutoField(primary_key=True)
     parent = models.ForeignKey(SalesFlatOrder, models.DO_NOTHING,
