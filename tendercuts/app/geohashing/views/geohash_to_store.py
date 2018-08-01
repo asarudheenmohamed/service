2f3744b9d14e3809d4b1dd89cea2cc15ae8ecd1e
Hariharan <hariharan@tendercuts.in>
2018-08-01 15:39:46 +0530

    review changes

--- tendercuts/app/geohashing/views/geohash_to_store.py
+++ tendercuts/app/geohashing/views/geohash_to_store.py
@@ -7,28 +7,31 @@ from rest_framework.response import Response
 from ..lib.geohash_controller import GeohashController
 
 
-
 class GeohashToStore(APIView):
     """
     Converts geohash to store_id. If geohash doesn`t
     work, then computes distance matrix for lat,lng
     and returns the corresponding store_id.
+    """
 
-    Params:
-    geohash(strring)
-    lat(number)
-    lng(number)
-
-    Url: geohash/store
-
-    Response: {'status': True, "store_id": None}
-    """  
     def post(self, request, format=None):
+        """
+        Params:
+        geohash(string): Geohash of customer address
+        lat(number): Latitude of customer address
+        lng(number): Longitude of customer address
+
+        Url: geohash/store
+
+        Returns: 
+        {'status': Boolean, "store_id": True}
+        """
 
         geohash = request.data["geohash"]
         lat = request.data["lat"]
         lng = request.data["lng"]
+
         controller = GeohashController()
-        status = controller.get_store_id(geohash,lat,lng)
+        status = controller.get_store_id(geohash, lat, lng)
 
         return Response(status)
