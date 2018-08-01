f0c022d8ab24b2c18fe042adea0f381de4fae8c6
asarudheen <asarudheen@tendercuts.in>
2018-08-01 17:25:18 +0530

    add task name in celery failure mail

--- tendercuts/app/core/lib/celery.py
+++ tendercuts/app/core/lib/celery.py
@@ -18,4 +18,4 @@ class TenderCutsTask(celery.Task):
     def on_failure(self, exc, task_id, args, kwargs, einfo):
         msg = '{0!r} failed: {1!r} errorinfo: {2!r}'.format(
             task_id, exc, einfo)
-        send_exception("[CRITICAL] Task failure", msg)
+        send_exception("[CRITICAL] {}-Task failure".format(self.name), msg)
