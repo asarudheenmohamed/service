f8249044d6d7baa1c0e2dd74ab3012dfcbb62de7
asarudheen <asarudheen@tendercuts.in>
2018-10-17 13:32:24 +0530

    enable adaya geofencing and userinline admin panel

--- tendercuts/app/core/admin.py
+++ tendercuts/app/core/admin.py
@@ -1,5 +1,7 @@
 # Register your models here.
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
 from app.core.models import UserProfile
 
@@ -9,3 +11,15 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 
 admin.site.register(UserProfile, UserProfileAdmin)
+
+admin.site.unregister(User)
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+
+class UserProfileInlineAdmin(UserAdmin):
+    inlines = [UserProfileInline, ]
+
+admin.site.register(User, UserProfileInlineAdmin)
